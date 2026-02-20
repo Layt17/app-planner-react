de1770b479fda9a6a6806fe26ca2d4c1d16aa8a0
Dmitry Dumanovsky <d.dumanovskii@onpoint.ru>
2026-02-20 16:22:12 +0300

    fix: color

--- src/components/header/header.c.tsx
+++ src/components/header/header.c.tsx
@@ -4,8 +4,12 @@ import { TgAppDataI } from "../../interfaces/tg-web-app-data.interface";
 export const HeaderC = ({ userInfo }: { userInfo: TgAppDataI }) => {
   return (
     <div id="header">
-      {userInfo.user.username}{', на дворе '} 
-      {state.weekInfo[0].date.toLocaleString(local, { month: "long" })}
+      <div className="header-content">
+        <div className="header-username">{userInfo.user.username}</div>
+        <div className="header-text">
+          на дворе {state.weekInfo[0].date.toLocaleString(local, { month: "long" })}
+        </div>
+      </div>
     </div>
   );
 };
